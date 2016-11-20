7e434197f66285ef0ea132eb76283cb8ec3d7bf1
aarexer <aarexer@gmail.com>
2016-11-20 21:11:38 +0300

    Common DAO interface

---
+++ src/main/java/com/github/aarexer/crud/dao/DAO.java
@@ -0,0 +1,53 @@
+package com.github.aarexer.crud.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+interface DAO<T> {
+    /**
+     * Add element to the database.
+     *
+     * @param element which would added.
+     * @throws SQLException if operation can't be executed.
+     */
+    void add(T element) throws SQLException;
+
+    /**
+     * Remove element by id from the database.
+     *
+     * @param id of element which would be removed.
+     * @throws SQLException if operation can't be executed.
+     */
+    void remove(int id) throws SQLException;
+
+    /**
+     * Get element with specify id from database.
+     * @param id of element which would be added.
+     * @return Element.
+     * @throws SQLException if operation can't be executed.
+     */
+    Optional<T> get(int id) throws SQLException;
+
+    /**
+     * Get all elements from database.
+     * @return all elements of in database
+     * @throws SQLException if operation can't be executed.
+     */
+    Optional<List<T>> getAll() throws SQLException;
+
+    /**
+     * Clear table.
+     *
+     * @throws SQLException if operation can't be executed.
+     */
+    void clear() throws SQLException;
+
+    /**
+     * Add elements to database.
+     *
+     * @param elements which would be added.
+     * @throws SQLException if operation can't be executed.
+     */
+    void addAll(List<T> elements) throws SQLException;
+}
