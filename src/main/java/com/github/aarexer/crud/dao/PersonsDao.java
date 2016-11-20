621fe90dc1432eadfd490dce4dc2a56f27b3f960
aarexer <aarexer@gmail.com>
2016-11-20 21:14:16 +0300

    Implementation of DAO for model

--- src/main/java/com/github/aarexer/crud/dao/PersonsDao.java
+++ src/main/java/com/github/aarexer/crud/dao/PersonsDao.java
@@ -1,13 +1,106 @@
 package com.github.aarexer.crud.dao;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.github.aarexer.crud.model.Person;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class PersonsDao {
-    public void createTablePersons(Connection conn) throws SQLException {
-        Statement stmt = conn.createStatement();
-        // create a new table
-        stmt.execute("CREATE TABLE IF NOT EXISTS PERSONS(ID INTEGER PRIMARY KEY, NAME TEXT, PHONE TEXT);");
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
+
+public class PersonsDao implements DAO<Person> {
+    private static final Logger logger = LogManager.getLogger();
+
+    private final Connection conn;
+    private final PreparedStatement removePrepStatement;
+    private final PreparedStatement addPrepStatement;
+    private final PreparedStatement getByIdPrepStatement;
+
+    public PersonsDao(Connection conn) {
+        this.conn = conn;
+
+        try {
+            this.removePrepStatement = conn.prepareStatement(Queries.StudentTable.REMOVE_BY_ID);
+            this.addPrepStatement = conn.prepareStatement(Queries.StudentTable.ADD);
+            this.getByIdPrepStatement = conn.prepareStatement(Queries.StudentTable.GET_BY_ID);
+        } catch (SQLException ex) {
+            logger.error("Can't create prepared statement");
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    public void createTable() throws SQLException {
+        logger.info("Trying to create table {}", Queries.StudentTable.TABLE_NAME);
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.execute(Queries.StudentTable.CREATE);
+
+            logger.info("Table {} created.", Queries.StudentTable.TABLE_NAME);
+        }
+    }
+
+    @Override
+    public void add(Person person) throws SQLException {
+        logger.info("Adding {}", person);
+
+        addPrepStatement.setLong(1, person.getId());
+        addPrepStatement.setString(2, person.getName());
+        addPrepStatement.setString(3, person.getPhone());
+
+        if (addPrepStatement.execute()) {
+            logger.info("Element {} added to {} table", person, Queries.StudentTable.TABLE_NAME);
+        } else {
+            logger.error("Can't add person {} to {} table", person, Queries.StudentTable.TABLE_NAME);
+        }
+    }
+
+    @Override
+    public void remove(int id) throws SQLException {
+        logger.info("Remove person by id: {}", id);
+
+        removePrepStatement.setLong(1, id);
+
+        if (removePrepStatement.executeUpdate() > 0) {
+            logger.info("Element with id {} removed from {} table", id, Queries.StudentTable.TABLE_NAME);
+        } else {
+            logger.error("Can't remove element with id {} from {} table", id, Queries.StudentTable.TABLE_NAME);
+        }
+
+        logger.info("Person with id: {} removed", id);
+    }
+
+    @Override
+    public Optional<Person> get(int id) throws SQLException {
+        logger.info("Get person by id: {}", id);
+
+        getByIdPrepStatement.setLong(1, id);
+
+        ResultSet res = getByIdPrepStatement.executeQuery();
+        if (res.next()) {
+            final String name = res.getString(2);
+            final String phone = res.getString(3);
+
+            logger.info("Person with id: {} returned", id);
+
+            return Optional.of(new Person(id, name, phone));
+        } else {
+            logger.info("Person with id: {} doesn't exist", id);
+            return Optional.empty();
+        }
+    }
+
+    //todo
+
+    @Override
+    public Optional<List<Person>> getAll() {
+        return null;
+    }
+
+    @Override
+    public void clear() throws SQLException {
+    }
+
+    @Override
+    public void addAll(List<Person> elements) throws SQLException {
     }
 }
---
+++ src/main/java/com/github/aarexer/crud/dao/Queries.java
@@ -0,0 +1,25 @@
+package com.github.aarexer.crud.dao;
+
+final class Queries {
+    private Queries() {
+        throw new AssertionError("Class for static can't have instances!");
+    }
+
+    static final class StudentTable {
+        private StudentTable() {
+            throw new AssertionError("Class for static can't have instances!");
+        }
+
+        static final String TABLE_NAME = "PERSONS";
+
+        static final String CREATE = String.format(
+                "CREATE TABLE IF NOT EXISTS %s(ID INTEGER PRIMARY KEY, NAME TEXT, PHONE TEXT);", TABLE_NAME
+        );
+
+        static final String ADD = String.format("INSERT INTO %s(ID, NAME, PHONE) VALUES(?, ?, ?);", TABLE_NAME);
+
+        static final String REMOVE_BY_ID = String.format("DELETE FROM %s WHERE ID=?;", TABLE_NAME);
+
+        static final String GET_BY_ID = String.format("SELECT * FROM %s WHERE ID=?;", TABLE_NAME);
+    }
+}
