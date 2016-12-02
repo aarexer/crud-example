699b82e61ce4a0e2e09d1052590db6e0bf6a3dfd
aarexer <aarexer@gmail.com>
2016-12-02 21:31:36 +0300

    Dummy tests

--- src/test/java/PersonsDaoTest.java
+++ src/test/java/PersonsDaoTest.java
@@ -1,21 +1,55 @@
 import com.github.aarexer.crud.DbConnection;
 import com.github.aarexer.crud.dao.PersonsDao;
+import com.github.aarexer.crud.model.Person;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.util.Optional;
+import static org.junit.Assert.*;
 
 public class PersonsDaoTest {
-    private PersonsDao personsDao;
+    private static PersonsDao personsDao;
 
     @BeforeClass
-    void setup() {
+    public static void beforeAllTests() {
         personsDao = new PersonsDao(DbConnection.getInstance().getConnection());
     }
 
-    @Test
-    public void createDataBaseTest() throws SQLException {
-        personsDao.createTable();
+    @AfterClass
+    public static void afterAllTests() throws SQLException {
+        DbConnection.getInstance().closeConnection();
+    }
 
+    @Test
+    public void getPersonFromTablePersonsById() throws SQLException{
+        Optional<Person> personFromDb = personsDao.get(1);
+
+        assertTrue(personFromDb.isPresent());
+        assertEquals("Testing", personFromDb.get().getName());
+        assertEquals("888", personFromDb.get().getPhone());
+        assertEquals(1, personFromDb.get().getId());
+    }
+
+    @Test
+    public void clearPersonsTable() throws SQLException {
+        personsDao.clear();
+
+        assertEquals(true, personsDao.getAll().isEmpty());
+    }
+
+    @Test
+    public void addPersonToTablePersons() throws SQLException {
+        Person personForTest = new Person(1, "Testing", "888");
+        personsDao.add(personForTest);
+    }
+
+    @Test
+    public void getAllPersonsFromTable() throws SQLException {
+        Person personForTest = new Person(1, "Testing", "888");
+        personsDao.add(personForTest);
+
+        assertEquals(false, personsDao.getAll().isEmpty());
     }
 }
